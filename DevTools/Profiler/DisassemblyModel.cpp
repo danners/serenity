019303f69f8e0f45fd1a1cae95d8a1668c75a11d
Simon Danner <danner.simon@gmail.com>
2021-01-09 13:31:21 +0100

    Profiler: fix sample count

--- DevTools/Profiler/DisassemblyModel.cpp
+++ DevTools/Profiler/DisassemblyModel.cpp
@@ -96,7 +96,7 @@ DisassemblyModel::DisassemblyModel(Profile& profile, ProfileNode& node)
         auto insn = disassembler.next();
         if (!insn.has_value())
             break;
-        FlatPtr address_in_profiled_program = symbol.value().value() + offset_into_symbol;
+        FlatPtr address_in_profiled_program = symbol.value().value() + offset_into_symbol + base_address;
 
         auto disassembly = insn.value().to_string(address_in_profiled_program, &symbol_provider);
 
